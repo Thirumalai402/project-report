7ec2013e65010e8bca4a095233fb83e8d5a0caec
Thirumalai402 <thirumalaideepak06@gmail.com>
2025-07-18 21:36:57 +0530

    Remove backend,controller,frontend, and templetes folders from main branch

--- backend/routes/order.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const { getorder } = require('../../controller/ordercontrol');
-const router = express.Router();
-
-router.route('/order').post(getorder);
-module.exports = router;
